43b6a3f17b12413b8b2f6a7cc3061b50027fbc16
Pirity Tamas <pirity.tom@gmail.com>
2022-04-23 22:45:57 +0200

    Remélem jó lesz így.

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -7,17 +7,20 @@ package main;
 public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
-        Ide jön a válaszod...
+        byte, short, int, long, float, double, char, boolean
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
-        Ide jön a válaszod...
+        a while ciklus előtesztelő ciklus, a do-while pedig hátultesztelő cilus.
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
-        Ide jön a válaszod...
+        Azonos típúsú adatok sorozatai.
+        Adattárolásra használhatóak.
+        Ha sok azonos típúsú adattal kell dolgoznunk.
+        Típus név [] létszám.
      */
 
 }
